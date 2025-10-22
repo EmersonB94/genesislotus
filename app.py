67fd3a9e2301e214c43985a4402ba031f4d80ec9
Emerson Borges <gestaodadosindicadores@gmail.com>
2025-10-22 10:12:55 -0300

    Update app.py
    Remoção da verificação do campo de senha

--- app.py
+++ app.py
@@ -89,7 +89,7 @@ def login():
     senha = data.get('senha')
 
     try:
-        result = supabase.table("usuariocad").select("*").eq("email", email).eq("senha", senha).execute()
+        result = supabase.table("usuariocad").select("*").eq("email", email).execute()
         if not result.data:
             return jsonify({"erro": "Usuário ou senha incorretos"}), 401
 
@@ -119,3 +119,4 @@ def usuario_html():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
