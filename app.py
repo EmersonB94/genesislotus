bc3e5802b510ead049107e5b7b59eb27df592db8
Emerson Borges <gestaodadosindicadores@gmail.com>
2025-10-22 10:15:49 -0300

    Update app.py

--- app.py
+++ app.py
@@ -89,7 +89,7 @@ def login():
     senha = data.get('senha')
 
     try:
-        result = supabase.table("usuariocad").select("*").eq("email", email).execute()
+        result = supabase.table("usuariocad").select("*").execute() # .eq("email", email).eq("senha", senha)
         if not result.data:
             return jsonify({"erro": "Usuário ou senha incorretos"}), 401
 
@@ -119,4 +119,5 @@ def usuario_html():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
 
